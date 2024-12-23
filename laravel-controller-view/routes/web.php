71d35df46e60c76f364197fd0501ca3577dc254d
Aurel79 <faurelia795@gmail.com>
2024-12-23 21:19:38 +0700

    modul 15

---
+++ laravel-controller-view/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\Auth\LoginController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// home kiri itu untuk akses url dan yang kanan untuk navbar yang ada di file home.blade.php/
+Route::get('home', [HomeController::class, 'index'])->name('home');
+Route::get('profile', ProfileController::class)->name('profile');
+Route::resource('employees', EmployeeController::class);
+Route::get('/employees/{employee}/edit', [EmployeeController::class, 'edit'])->name('employees.edit');
+Route::put('/employees/{employee}', [EmployeeController::class, 'update'])->name('employees.update');
+
+
+Auth::routes(['register' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::resource('/employees', EmployeeController::class);
+});
+
+Route::get('/', function () {
+    return redirect('/login');
+});
+
+
+
+
